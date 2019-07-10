b232072408f2da5484f2a4fa5fccbd74d9abbaf1
Federico Galatolo <galatolo.federico@gmail.com>
2019-07-10 15:58:47 +0200

    Example with actual unbalanced dataset

--- example.py
+++ example.py
@@ -1,14 +1,16 @@
 import torch
-from torchvision import datasets, transforms
 from sampler import BalancedBatchSampler
 
-train_dataset = datasets.MNIST("/tmp/mnist_data",
-                train=True, download=True, transform=transforms.Compose([
-                transforms.ToTensor(),
-            ])
-        )
+size = 20
+features = 5
+classes_prob = torch.tensor([0.1, 0.4, 0.5])
 
-train_loader = torch.utils.data.DataLoader(train_dataset, sampler=BalancedBatchSampler(train_dataset), batch_size=30)
+dataset_X = torch.randn(size, features)
+dataset_Y = torch.distributions.categorical.Categorical(classes_prob.repeat(size, 1)).sample()
+
+dataset = torch.utils.data.TensorDataset(dataset_X, dataset_Y)
+
+train_loader = torch.utils.data.DataLoader(dataset, sampler=BalancedBatchSampler(dataset, dataset_Y), batch_size=6)
 
 for batch_x, batch_y in train_loader:
-    print(batch_y)
+    print("labels: %s\ninputs: %s\n" % (batch_y, batch_x))
