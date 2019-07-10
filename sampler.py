94a6f970274be57f674e154635342e64caf6e70b
Federico Galatolo <galatolo.federico@gmail.com>
2019-07-10 15:56:00 +0200

    Use the vale of the tensor as key, not the tensor

--- sampler.py
+++ sampler.py
@@ -6,13 +6,13 @@ import random
 class BalancedBatchSampler(torch.utils.data.sampler.Sampler):
     def __init__(self, dataset, labels=None):
         self.labels = labels
-        self.dataset = {}
+        self.dataset = dict()
         self.balanced_max = 0
         # Save all the indices for all the classes
         for idx in range(0, len(dataset)):
             label = self._get_label(dataset, idx)
             if label not in self.dataset:
-                self.dataset[label] = []
+                self.dataset[label] = list()
             self.dataset[label].append(idx)
             self.balanced_max = len(self.dataset[label]) \
                 if len(self.dataset[label]) > self.balanced_max else self.balanced_max
@@ -21,7 +21,6 @@ class BalancedBatchSampler(torch.utils.data.sampler.Sampler):
         for label in self.dataset:
             while len(self.dataset[label]) < self.balanced_max:
                 self.dataset[label].append(random.choice(self.dataset[label]))
-    
         self.keys = list(self.dataset.keys())
         self.currentkey = 0
 
@@ -33,7 +32,7 @@ class BalancedBatchSampler(torch.utils.data.sampler.Sampler):
     
     def _get_label(self, dataset, idx, labels = None):
         if self.labels is not None:
-            return self.labels[idx]
+            return self.labels[idx].item()
         else:
             # Trying guessing
             dataset_type = type(dataset)
